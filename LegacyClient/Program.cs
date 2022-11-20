ee52b6e05cf226ef98d1706cb73ac00756035d87
Nicolas MANGUÉ <nicolas.mangue@gmail.com>
2022-11-20 20:48:42 +0100

    Initial commit

---
+++ LegacyClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using LegacyClient.ServiceReference;
+using System;
+using System.ServiceModel;
+
+namespace LegacyClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to run...");
+            Console.ReadKey();
+
+            using (var serviceClient = new ServiceClient())
+            {
+                serviceClient.Open();
+                var result = serviceClient.GetData(32);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,35 @@
+﻿using CoreWCF;
+using CoreWCF.Configuration;
+using CoreWCF.Description;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceModelServices();
+            services.AddServiceModelMetadata();
+            services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseServiceModel(serviceBuilder =>
+            {
+                var ntWsBinding = new WSHttpBinding(SecurityMode.TransportWithMessageCredential);
+                ntWsBinding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+
+                serviceBuilder.AddService<Service>();
+                serviceBuilder.AddServiceEndpoint<Service, IService>(ntWsBinding, "/Service.svc");
+                var serviceMetadataBehavior = app.ApplicationServices.GetRequiredService<ServiceMetadataBehavior>();
+                serviceMetadataBehavior.HttpsGetEnabled = true;
+            });
+        }
+    }
+}
